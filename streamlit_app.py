654d4d61cf03fdaeb86877767d002ead17c5f934
Bandarunaveen-tech <98140112+Bandarunaveen-tech@users.noreply.github.com>
2023-06-26 14:30:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit
 
 streamlit.title('my parents new healthy dinner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & bluberry oatemal')
 streamlit.text('kale,s[pinach & Rocket Smoothie')
 streamlit.text('Hard-boiled free-range egg')
+
+streamlit.header(' build your own fruit smoothie')
