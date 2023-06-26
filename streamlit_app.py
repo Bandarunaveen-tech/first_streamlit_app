416b098e211c89132245fc70ab3869ac8cb42826
Bandarunaveen-tech <98140112+Bandarunaveen-tech@users.noreply.github.com>
2023-06-26 14:36:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('Hard-boiled free-range egg')
 
 streamlit.header(' build your own fruit smoothie')
 
-// pandas 
+ ///pandas 
 import pandas
 my_fruit_list =pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
