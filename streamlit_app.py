25b5c65cb72f098ece33203517a98257503b64bb
Bandarunaveen-tech <98140112+Bandarunaveen-tech@users.noreply.github.com>
2023-06-26 14:36:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ streamlit.text('kale,s[pinach & Rocket Smoothie')
 streamlit.text('Hard-boiled free-range egg')
 
 streamlit.header(' build your own fruit smoothie')
-
- ///pandas 
+ 
 import pandas
 my_fruit_list =pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
