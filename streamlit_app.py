b51c6b0deeff4428458367fc9da66375dfb45037
Bandarunaveen-tech <98140112+Bandarunaveen-tech@users.noreply.github.com>
2023-06-26 15:01:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ my_fruit_list =pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.inedx))
                                         
 
 
